eadabbbac7d19f6b73474ef6f237c1f7a12b652e
Uday Shergill <86528848+udayshergill@users.noreply.github.com>
2021-06-27 11:31:58 -0400

    Add files via upload
    Adding numbers with a while loop, sleep function, int variables, string variables, and inputs with text.

---
+++ AddingNumbers.py
@@ -0,0 +1,13 @@
+import time
+numberint = 0
+int(numberint)
+print("Hello I will be adding your numbers\n")
+time.sleep(0.5)
+numberamount = int(input("How many numbers are you adding?\n"))
+time.sleep(0.5)
+while numberamount>0:
+    newnumber = int(input("What is the number you want to add\n"))
+    time.sleep(0.1)
+    numberint = numberint + newnumber
+    numberamount = numberamount -1
+print("Your number is " + str(numberint))
