55c6c88231fdcacef4ecf6d4c40143f3737e8e2a
Uday Shergill <86528848+udayshergill@users.noreply.github.com>
2021-06-29 11:07:02 -0400

    Update stringspliter.py

--- stringspliter.py
+++ stringspliter.py
@@ -4,11 +4,9 @@ def stringspliter(sentence):
     print(sentence)
     words = sentence.split(" ")
     word_amount = len(words)
-    print(words)
-    print(word_amount)
+    print("Your sentence has " + len + " amount of words")
     while holder != word_amount:
         print(words[holder])
         holder += 1
-
-
+        
 stringspliter(input("Input a sentence that I will break apart\n"))
