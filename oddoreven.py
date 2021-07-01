1032682f9183e4880b341f1d037a5db9d23884d3
Uday Shergill <86528848+udayshergill@users.noreply.github.com>
2021-07-01 10:51:08 -0400

    Add files via upload
    Odd or even finder function using a modulo operator (%) and else.

---
+++ oddoreven.py
@@ -0,0 +1,7 @@
+def oddoreven(number):
+    if (number % 2) == 0:
+        print("The number is even")
+    else:
+         print("The number is odd")
+
+oddoreven(int(input("Please provide a number to determine if its odd or even\n")))
