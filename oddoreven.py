0aaf8eace90f5f7b3ebe76ca378a0c46258f5498
Uday Shergill <86528848+udayshergill@users.noreply.github.com>
2021-07-01 10:52:20 -0400

    Update oddoreven.py

--- oddoreven.py
+++ oddoreven.py
@@ -1,7 +1,6 @@
 def oddoreven(number):
     if (number % 2) == 0:
-        print("The number is even")
+        return(print("The number is even"))
     else:
-         print("The number is odd")
-
+         return(print("The number is odd"))
 oddoreven(int(input("Please provide a number to determine if its odd or even\n")))
