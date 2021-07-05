0163375f79826301c130238afa149a8e6da91170
Uday Shergill <86528848+udayshergill@users.noreply.github.com>
2021-07-05 16:31:52 -0400

    Update Desk_calculator.py

--- Desk_calculator.py
+++ Desk_calculator.py
@@ -6,8 +6,8 @@ def cost(numd, wood):
       return 200
     elif (numd > 3):
       oakamount = numd - 3 
-      oakamount = oakamount* 25
-      oakamount = oakamount + 200
+      oakamount *= 25
+      oakamount += 200
       print("The Oak Desk cost {} Dollars\n" .format(oakamount))
       return oakamount
   if (wood == "mohagany"):
@@ -16,8 +16,8 @@ def cost(numd, wood):
      return 300
    elif(numd > 3):
      mohaganyamount = numd - 3
-     mohaganyamount = mohaganyamount * 45
-     mohaganyamount = mohaganyamount + 300
+     mohaganyamount *= 45
+     mohaganyamount += 300
      print("This Mohagany Desk costs {} Dollars\n" .format(mohaganyamount))
      return mohaganyamount
   if (wood == "hickory"):
@@ -26,17 +26,17 @@ def cost(numd, wood):
      return 400
     elif(numd > 3):
      hickoryamount = numd -3
-     hickoryamount = hickoryamount * 60
-     hickoryamount = hickoryamount + 400
+     hickoryamount *= 60
+     hickoryamount += 400
      print("The cost of this Hickory desk is {} Dollars\n" .format(hickoryamount))
      return hickoryamount
 
 print("Hello I will caculate the cost of your desk with your given parameters\n")
-time.sleep(1.25)
+time.sleep(1)
 numd = int(input("How many drawers would you want?\n"))
-time.sleep(1.25)
+time.sleep(1)
 wood = input("Would you like an oak, mohagany, or hickory desk?\n")
-time.sleep(1.25)
+time.sleep(1)
 cost(numd, wood)
 
-input("Press enter to close\n")
+input("Press enter to close\n") #for exe verison
